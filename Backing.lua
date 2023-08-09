1564857b215598a75594adc48ea2083f4fa6890e
false-commits <132003518+false-commits@users.noreply.github.com>
2023-08-09 00:21:49 -0700

    Update Backing.lua

--- Backing.lua
+++ Backing.lua
@@ -1,12 +1,12 @@
 -- CREDIT RayField 
--- By KR47Y
+-- By lmmortalz on discord
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 
 local Window = Rayfield:CreateWindow({
 	Name = "🔥| Backdoor Hubs",
 	LoadingTitle = "Backdoor Hubs",
-	LoadingSubtitle = "by KR47Y",
+	LoadingSubtitle = "by lmmortalz on discord",
 	ConfigurationSaving = {
 		Enabled = false,
 		FolderName = nil, 
